e8192f0554cf44fa83ac1b428f19940a964358c5
James <james.zero4@gmail.com>
2024-05-09 16:05:40 +0700

    update

--- app/components/Section.jsx
+++ app/components/Section.jsx
@@ -11,8 +11,8 @@ export default function Section({session}) {
                 Welcome <p className='text-blue-800 text-center'>Booking</p>
               </h1>
               <br></br>
-              <p className='text-xl text-center text-black-300 my-1'>We love the Sea</p>
-              <p className='text-xl text-center text-black-300 my-2'>We love the Islands</p>
+              <p className='text-xl text-center text-black-300 my-1'>We love the Sea !!</p>
+              <p className='text-xl text-center text-black-300 my-2'>We love the Islands !!!</p>
           </div>
         </div>
  
